4f8885ce808969362b9cfa9bedaa67fb391ded13
Luan Santos <44438249+lvluanvinicius@users.noreply.github.com>
2022-08-28 21:23:18 -0300

    Update index.php

--- Loading Host Settings/index.php
+++ Loading Host Settings/index.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$file_name="pmOurinhos.geojson";
+$file_name="filename.geojson";
 
 $json_file = fopen($file_name, "r") or die("\nImpossível abrir o arquivo $file_name\n");
 $file_data = json_decode(fread($json_file, filesize($file_name)));
